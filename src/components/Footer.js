b20565d2dce880d383c23e9099bbd8acce945442
Ivan Fedorushchenko <109195438+ivanfedoruschenko@users.noreply.github.com>
2023-06-13 12:36:22 +0300

    deploy

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return(
-        <footer className="footer">
-            <p className="footer__copyright">&copy;2022 Mesto Russia</p>
-        </footer>
-    )
-}
